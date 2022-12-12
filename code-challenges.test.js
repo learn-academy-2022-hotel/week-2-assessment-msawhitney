6f2d09bca5a78239241ebb85e535d95c6ce5c843
msawhitney <msawhit3@gmail.com>
2022-12-12 08:25:19 -0800

    assessment code challenges

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -23,6 +23,13 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
+Pseudocode
+Input: object with one key-value pair
+Output: a string indicating if the value in the object is divisible by 3.
+Process: Access the number value of each object using dot notation. Use modulo to determine if the number is divisible by three. I have gotten parts of this to work but can't get it to work all together. I keep getting an error.
+Expected output: below
+Actual output:
+
 // a) Create a test with expect statements for each of the variables provided.
 
 const object1 = { number: 15 }
@@ -36,16 +43,43 @@ const object3 = { number: -7 }
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
+// Pseudocode
+// Input: array of strings
+// Output: array of strings with value at the zero index position of each string capitalized.
+// Process: Declare a function called capitalWords with array as an argument.
+// Access each element and turn the character at the zero index of each element to a capital letter, turn the characters from the one index on to lowercase and add them back together.
+//
+// const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+// // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+// Actual output: [ 'Streetlamp', 'Potato', 'Teeth', 'Conclusion', 'Nephew' ]
+// const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
+// // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+// Actual output: [ 'Temperature', 'Database', 'Chopsticks', 'Mango' ]
+// // b) Create the function that makes the test pass.
+//
+// const capitalWords = (array) => {
+//   return array.map(element => {
+//     return element.charAt(0).toUpperCase() + element.slice(1).toLowerCase();
+//   });
+// }
+//
+// console.log(capitalWords(randomNouns1));
+// console.log(capitalWords(randomNouns2));
+
 // a) Create a test with expect statements for each of the variables provided.
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
 
-// b) Create the function that makes the test pass.
+
+
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
+
+Pseudocode
+Input: string
+Output: number (index number of the first vowel in the string)
+Process:use indexOf to find vowels in the string. I have had trouble trying to figure out how to run through all the vowels. I can find the first a and then the first e, etc.
+Expected output: below
+Actual output:
 
 // a) Create a test with expect statements for each of the variables provided.
 
@@ -57,3 +91,9 @@ const vowelTester3 = "challenges"
 // Expected output: 2
 
 // b) Create the function that makes the test pass.
+
+const vowelFinda = (string) => {
+  return string.indexOf("a")
+}
+console.log(vowelFind (vowelTester1))
+//console.log(vowelTester1.split(""))
